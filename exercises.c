9221c3479cc8370268331caffbabf88096029906
Your Name <you@example.com>
2025-03-19 08:48:42 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -45,7 +45,7 @@ List* crea_lista() {
    List* L = create_list();
    int numeros[10] = {1,2,3,4,5,6,7,8,9,10};
    for(int i = 0; i < 10; i++){
-      pushFront(L, &numeros[i]);
+      pushBack(L, &numeros[i]);
    }
    return L;
 }
