455d2ce075e53e4f3e536c2963f6e9f3a46f4cf3
Your Name <you@example.com>
2025-03-20 18:53:49 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -136,10 +136,7 @@ int parentesisBalanceados(char *cadena){
          if(cadena[i] == ')' && strcmp(top(pila),"(") != 0 ||
             cadena[i] == '}' && strcmp(top(pila),"{") != 0 ||
             cadena[i] == ']' && strcmp(top(pila),"[") != 0 ) return 0;
-         /*if(cadena[i] == ')' && top(pila) == '(' || 
-            cadena[i] == '}' && top(pila) == '{' || 
-            cadena[i] == '}' && top(pila) != '{') pop(pila);
-         */
+         
          else return 0;
       }
    }
